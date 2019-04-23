613fc6c7c0e188eb4e7e5688e9b1114e203563d9
Hanyu <hanyutang.sandra@gmail.com>
2019-04-23 02:10:57 -0400

    Updates

---
+++ precache-manifest.2cbf0cff28c90f0c5d603d608dcf052d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fa3bcf413d88da3e0997b67da2f4641",
+    "url": "/ASR-prototype/index.html"
+  },
+  {
+    "revision": "d2049cfa41e5122d2038",
+    "url": "/ASR-prototype/static/js/2.f24fee81.chunk.js"
+  },
+  {
+    "revision": "767b274b89085fe882cf",
+    "url": "/ASR-prototype/static/js/main.693e3332.chunk.js"
+  },
+  {
+    "revision": "212ae878db7d730793eb",
+    "url": "/ASR-prototype/static/js/runtime~main.e0a6cf33.js"
+  },
+  {
+    "revision": "67310b77b3e4b3cd327ea072caa11b13",
+    "url": "/ASR-prototype/static/media/logo.67310b77.png"
+  }
+]);
